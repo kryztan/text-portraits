3108e5589af512bc45c573bb52341a36798cbe29
Kryztan Santos <kryztan.s@gmail.com>
2022-07-05 23:29:37 +0800

    Add workout page

---
+++ resources/views/workout/index.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <div>Workout</div>
+
+    <textarea id="workouts" name="workouts" rows="5 cols="50">
+        Workout 7/5
+        bench press
+        50w 10r (3x)
+    </textarea>
+
+    <button type="button">Submit</button>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TextPortraitsController;
+use App\Http\Controllers\WorkoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('text-portraits', [TextPortraitsController::class, 'index'])->name('text-portraits.index');
+
+Route::get('workout', [WorkoutController::class, 'index'])->name('workout.index');
