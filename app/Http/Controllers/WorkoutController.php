304ffdbd3e1825e0d7b70f21b9281672d82869eb
Kryztan Santos <kryztan.s@gmail.com>
2022-07-13 22:22:35 +0800

    Add Workout query

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -17,26 +17,80 @@ class WorkoutController extends Controller
         $text = $request->text;
         $lines = preg_split('/\r\n|\r|\n/', $text);
 
-        $workout_ids = [];
-
+        // Parse text
+        $exercises = [];
+        $curr_exercise_index = -1;
+        $exercise_sets = [];
+        $before_exercise = false;
+        $before_set = false;
         foreach ($lines as $index => $line) {
+            $line = trim($line);
+
+            // Empty line
+            if ($line == "") {
+                $before_exercise = true;
+                $before_set = false;
+                continue;
+            }
+            // Get workout name
             if ($index === array_key_first($lines)) {
                 $name = $line;
+                continue;
+            } elseif (!isset($name)) {
+                // No workout name
+                break;
+            }
+
+            // Get exercise
+            if ($before_exercise) {
+                $exercises[] = $line;
+                $curr_exercise_index++;
+                $exercise_sets[$curr_exercise_index] = [];
+
+                $before_exercise = false;
+                $before_set = true;
+            } elseif ($before_set) {
+                // Get exercise set
+                $exercise_sets[$curr_exercise_index][] = [
+                    'set' => $line
+                ];
             }
         }
 
+        // Insert workout
         if (isset($name)) {
             $workout_id = DB::table('workouts')->insertGetId([
                 'name' => $name,
                 'user_id' => 1,
+                'date' => '2022-07-13',
                 'description' => $text
             ]);
 
-            $workout_ids[] = $workout_id;
+            // Insert exercises
+            if ($workout_id) {
+                foreach ($exercises as $eIndex => $exercise) {
+                    $workout_exercise_id = DB::table('workout_exercises')->insertGetId([
+                        'exercise_id' => 1,
+                        'exercise' => $exercise
+                    ]);
+
+                    // Insert sets
+                    if ($workout_exercise_id) {
+                        foreach ($exercise_sets[$eIndex] as $sIndex => $exercise_set) {
+                            DB::table('workout_exercise_sets')->insert([
+                                'workout_exercise_id' => $workout_exercise_id,
+                                'number' => $sIndex + 1,
+                                'weight' => $exercise_set,
+                                'reps' => 1
+                            ]);
+                        }
+                    }
+                }
+            }
         }
 
         return response()->json([
-            'workout_ids' => $workout_ids,
+            'workout_id' => $workout_id ?? 0,
             'text' => $text,
         ]);
     }
