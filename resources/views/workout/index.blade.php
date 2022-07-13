faf69f6335e21ddd69d255bd491950de383caac9
Kryztan Santos <kryztan.s@gmail.com>
2022-07-13 20:09:22 +0800

    Add Workout query

--- resources/views/workout/index.blade.php
+++ resources/views/workout/index.blade.php
@@ -24,6 +24,7 @@
             data: { yo: 8 },
             dataType: "json"
         }).done(function(data) {
+            console.log(data);
             let response = $.parseJSON(data);
 
             console.log(response);
