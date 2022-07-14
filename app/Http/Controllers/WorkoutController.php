7b1fb995297d09d0b01a623a2d02b6567e4702c6
Kryztan Santos <kryztan.s@gmail.com>
2022-07-14 22:36:02 +0800

    Parse set details

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class WorkoutController extends Controller
 {
@@ -79,15 +80,21 @@ class WorkoutController extends Controller
                     // Insert sets
                     if ($workout_exercise_id) {
                         foreach ($exercise_sets[$eIndex] as $sIndex => $exercise_set) {
+                            Log::debug('Inserting set 1');
+
                             $set = $this->parseSet($exercise_set);
 
+                            Log::debug('Inserting set 2');
+
                             for ($i = 0; $i < $set['times']; $i++) {
+                                Log::debug('Inserting set 3');
                                 DB::table('workout_exercise_sets')->insert([
                                     'workout_exercise_id' => $workout_exercise_id,
                                     'number' => $sIndex + 1 + $i,
                                     'weight' => $set['weight'],
                                     'reps' => $set['reps']
                                 ]);
+                                Log::debug('Inserting set 4');
                             }
                         }
                     }
