c618e74bbe3815e19d5fb7df367479486773371c
Kryztan Santos <kryztan.s@gmail.com>
2022-08-28 03:23:09 +0800

    Add workout table

--- resources/views/workout/index.blade.php
+++ resources/views/workout/index.blade.php
@@ -40,17 +40,19 @@
                 <tr>
                     <td colspan="3">{{ $workout->name }}</td>
                 </tr>
-{{--                @foreach ($workout->workout_exercise as $workout_exercise)--}}
-{{--                    @foreach ($workout_exercise->workout_exercise_sets as $workout_exercise_sets)--}}
-{{--                        <tr>--}}
-{{--                            @if ($workout_exercise_sets->number === 1)--}}
-{{--                                <td rowspan="{{ count($workout_exercise) }}">{{ $workout_exercise->exercise }}</td>--}}
-{{--                            @endif--}}
-{{--                            <td>{{ $workout_exercise_sets->weight }}</td>--}}
-{{--                            <td>{{ $workout_exercise_sets->reps }} {{ $workout_exercise_sets->description }}</td>--}}
-{{--                        </tr>--}}
-{{--                    @endforeach--}}
-{{--                @endforeach--}}
+                @if ($loop->first)
+                    @foreach ($workout->workout_exercise as $workout_exercise)
+                        @foreach ($workout_exercise->workout_exercise_sets as $workout_exercise_sets)
+                            <tr>
+                                @if ($workout_exercise_sets->number === 1)
+                                    <td rowspan="{{ count($workout_exercise) }}">{{ $workout_exercise->exercise }}</td>
+                                @endif
+                                <td>{{ $workout_exercise_sets->weight }}</td>
+                                <td>{{ $workout_exercise_sets->reps }} {{ $workout_exercise_sets->description }}</td>
+                            </tr>
+                        @endforeach
+                    @endforeach
+                @endif
             </table>
         @endforeach
     </div>
