61c03b4339526909310cca36ed098151a77de18d
Kryztan Santos <kryztan.s@gmail.com>
2022-08-28 03:13:55 +0800

    Add workout table

--- resources/views/workout/index.blade.php
+++ resources/views/workout/index.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
     <textarea class="workouts" name="workouts" rows="5 cols="50"></textarea>
-
     <button type="button" class="submit">Submit!</button>
 
     <div style="margin: 30px">
@@ -36,41 +35,30 @@
             </tr>
         </table>
 
-        <table class="table table-bordered table-light">
-            <tr>
-                <td colspan="3">Workout 7 29 22</td>
-            </tr>
-            <tr>
-                <td rowspan="3">bench press</td>
-                <td>45w</td>
-                <td>10r</td>
-            </tr>
-            <tr>
-                <td>50w</td>
-                <td>9r</td>
-            </tr>
-            <tr>
-                <td>50w</td>
-                <td>10r</td>
-            </tr>
-            <tr>
-                <td rowspan="3">barbell row</td>
-                <td>45w</td>
-                <td>12r -desc</td>
-            </tr>
-            <tr>
-                <td>50w</td>
-                <td>8r -desc</td>
-            </tr>
-            <tr>
-                <td>45w</td>
-                <td>12r</td>
-            </tr>
-        </table>
+        @foreach ($workouts as $workout)
+            <table class="table table-bordered table-light">
+                <tr>
+                    <td colspan="3">{{ $workout->name }}</td>
+                </tr>
+                @foreach ($workout->workout_exercise as $workout_exercise)
+                    @foreach ($workout_exercise->workout_exercise_sets as $workout_exercise_sets)
+                        <tr>
+                            @if ($workout_exercise_sets->number === 1)
+                                <td rowspan="{{ count($workout_exercise) }}">{{ $workout_exercise->exercise }}</td>
+                            @endif
+                            <td>{{ $workout_exercise_sets->weight }}</td>
+                            <td>{{ $workout_exercise_sets->reps }} {{ $workout_exercise_sets->description }}</td>
+                        </tr>
+                    @endforeach
+                @endforeach
+            </table>
+        @endforeach
     </div>
 </x-layout>
 
 <script>
+    console.log(<?= json_encode($workouts) ?>);
+
     $.ajaxSetup({
         headers: {
             'X-CSRF-Token': "{{ csrf_token() }}"
@@ -89,6 +77,4 @@
             console.log(data);
         });
     });
-
-    console.log(<?= json_encode($workouts) ?>);
 </script>
